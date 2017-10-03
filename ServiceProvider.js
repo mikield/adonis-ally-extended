9028e3ed25afc07b65613e728988c5b8cd02c6d3
Vladyslav Gaysyuk <mikield@icloud.com>
2017-10-04 01:34:16 +0200

    init

---
+++ ServiceProvider.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/*
+* adonis-auth
+*
+* (c) Vladyslav Gaysyuk <mikield@icloud.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+const { ServiceProvider } = require('@adonisjs/fold')
+const _ = require('lodash')
+
+const Drivers = {
+  vk: require('./Vk')
+}
+
+class AllyServiceProvider extends ServiceProvider {
+  /**
+  * The register method called by ioc container
+  * as a life-cycle method
+  *
+  * @method register
+  *
+  * @return {void}
+  */
+  boot () {
+    _.forEach(Drivers, (implementation, name) => {
+      this.app.extend('Adonis/Addons/Ally', name, () => {
+        return this.app.make(implementation)
+      })
+    })
+
+  }
+}
+
+module.exports = AllyServiceProvider
